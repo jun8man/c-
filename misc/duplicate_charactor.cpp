91b0ced44861b6733339c951a64081fdb0165ab4
Junya Yamashita <Junya.Yamashita@jp.sony.com>
2015-06-14 16:40:13 +0530

    Modify comment sentence.

--- misc/duplicate_charactor.cpp
+++ misc/duplicate_charactor.cpp
@@ -16,7 +16,7 @@ is_there_ducplicate_charactor(const std::string& str)
     for (int i = 0; i < str.size(); i++)
     {
         /**
-         * ぼくはこう書いた.
+         * Initial code.
          */
         /*
         for (int j = i + 1; j < str.size(); j++)
@@ -25,12 +25,16 @@ is_there_ducplicate_charactor(const std::string& str)
         }
         */
        /**
-        * 仲さんからはstlを使うと1行で書けると聞いてこちらを書いた.
+        * stlを使うと1行で書けると聞いたのでこんなのを考えた.
         * setは重複を許さないため通常同じものをinsertしようとすると無視される.
         * 実際にはpair<iterator, bool>を返し、1番目は挿入した値へのIteratorで
         * 2番めは挿入が実際に行われたかどうかを示す.
+        *
+        * ちょっと分かりにくいのでfindを使う手段を教わった.
+        * findは要素があればそのIteratorを返す.無ければ終端を返すので、それを利用する.
         */
-        if (!s.insert(str[i]).second) return true;
+        if (s.find(str[i]) != s.end()) return true;
+        s.insert(str[i]);
         std::cout << str[i] << std::endl;
     }
     return false;
