4ade700d3bd570b9efaefcf8eafb72b6d65a11a9
Junya Yamashita <Junya.Yamashita@jp.sony.com>
2015-06-14 16:42:25 +0530

    Add sample code of API call
    To test API implementation, try to write an API.

--- simple_delegate/simple_delegate.cpp
+++ simple_delegate/simple_delegate.cpp
@@ -25,11 +25,11 @@ public:
   // コンストラクタを記載すると、必ず実体の定義を書かねばならなくなる
   Delegate(){}
   virtual ~Delegate(){}
-  virtual void operator()() = 0;
+  virtual void operator()(const int i) = 0;
 };
 
-void func(){
-  std::cout << "This is Normal func()" << std::endl;
+void func(const int i){
+  std::cout << "This is Normal func() i = " << i << std::endl;
 }
 
 class My_class
@@ -40,35 +40,53 @@ public:
   ~My_class(){
   }
 
-  void func(){
-    std::cout << "This is My_class::func()" << std::endl;
+  void func(const int i){
+    m_val = i;
+    std::cout << "This is My_class::func():: m_val = " << m_val << std::endl;
   }
+
+  int get_val() const
+  {
+    return m_val;
+  }
+
+private:
+  int m_val;
 };
 
 class Simple_func
   : public Delegate
 {
 public:
-  Simple_func(void (*call_back)())
-   : m_call_back(call_back){       // 戻り値の型 (* 引数名)(関数の取り得る引数)
+  typedef void (*call_back)(const int);
+
+  Simple_func(call_back func)
+   : m_call_back(func){
+  //Simple_func(void (*call_back)(const int i))
+  // : m_call_back(call_back){       // 戻り値の型 (* 引数名)(関数の取り得る引数)
   }
   ~Simple_func(){
   }
 
-  virtual void operator()() {
-      (*m_call_back)();
+  virtual void operator()(const int i)
+  {
+    (*m_call_back)(i);
   }
 
 private:
-  void (*m_call_back)();
+  call_back m_call_back;
+  //void (*m_call_back)(const int i);
 };
 
-template<typename T_Class>
+
+template<typename T_Class >
 class Simple_class
   : public Delegate
 {
 public:
-  Simple_class(T_Class* obj, void (T_Class::*call_back)())   // 戻り値の型 (クラス名::*変数名)(関数の取り得る引数)
+  typedef void (T_Class::*call_back_t)(const int);
+  Simple_class(T_Class* obj, call_back_t call_back)
+  //Simple_class(T_Class* obj, void (T_Class::*call_back)(const int i))   // 戻り値の型 (クラス名::*変数名)(関数の取り得る引数)
    : m_call_back(call_back)
    , m_obj(obj){
   }
@@ -76,18 +94,24 @@ public:
   }
 
   // 最初の()はコンストラクタ的な意味、で後の()が関数の引数部分的な意味
-  virtual void operator()() {
-      (m_obj->*m_call_back)();
+  virtual void operator()(const int i) {
+      (m_obj->*m_call_back)(i);
   }
 
 private:
-  void (T_Class::*m_call_back)();
+  call_back_t m_call_back;
+  //void (T_Class::*m_call_back)(const int i);
   T_Class *m_obj;
 };
 
 void api_yamajun(Delegate& call_back)
 {
-  call_back();
+  /**
+   * ここでいろいろ処理した結果をAPIの呼び出し元に返してあげる
+   */
+  int i = 0;
+  i++;
+  call_back(i);
 }
 
 int main(int argc, char const *argv[])
@@ -96,7 +120,7 @@ int main(int argc, char const *argv[])
   // 型はTemplateを使っているのでなんでもよい
   // 受ける側は継承元のインターフェイスクラス
   My_class obj;
-  Simple_class<My_class> obj2(&obj, &My_class::func);
+  Simple_class<My_class > obj2(&obj, &My_class::func);
   // *は()よりも呼び出し優先度が低いので*obj2()と書くと*(obj2())ってことになってエラーになるので注意！
   //  (*obj2)();
 
@@ -106,7 +130,8 @@ int main(int argc, char const *argv[])
   //  (*obj3)();
 
   api_yamajun(obj2);
-  api_yamajun(obj3);
+  std::cout << obj.get_val() << std::endl;
 
+  api_yamajun(obj3);
   return 0;
 }
