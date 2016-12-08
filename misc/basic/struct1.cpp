028150a79ab237a7f58fd0f766265c2613e7e000
junya <junya0220yamashita@gmail.com>
2016-12-08 23:18:50 +0530

    update basic study

--- misc/basic/struct1.cpp
+++ misc/basic/struct1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+        #include <iostream>
 
 #define SIZE_OF_ARRAY(array)    sizeof (array) /sizeof *(array)
 
@@ -11,14 +11,25 @@ struct Student
     int jananese, math, english;
 };
 
-// 構造体は大きいので参照渡しがよい. 
-// ポインタで渡す案もあり
-void display(const Student& student)
+// 構造体はデータ量が多いので参照渡しがよい.
+// 値渡しにすると構造体がコピーされてしまう.
+void display_refer(const Student& student)
 {
-    cout << "name: " << student.name << endl
+    cout << "[display_refer]" << endl
+         << "name: " << student.name << endl
          << "Japanese: " << student.jananese << ", "
          << "math: " << student.jananese << ", "
          << "english: " << student.jananese << endl;
+}
+
+// ポインタで渡す案もあり.
+void display_pointer(const Student* student)
+{
+    cout << "[display_pointer]" << endl
+         << "name: " << student->name << endl
+         << "Japanese: " << student->jananese << ", "
+         << "math: " << student->jananese << ", "
+         << "english: " << student->jananese << endl;
 }
 
 int main(int argc, char const *argv[])
@@ -28,9 +39,28 @@ int main(int argc, char const *argv[])
         {"Kana", 90, 92, 100},
         {"Toma", 100, 100, 100}
     };
+
+    /*
+        cだと変数宣言時も"struct"を書かないといけない.
+        sturct Student student[] = {
+            {"Junya", 73, 98, 86},
+            {"Kana", 90, 92, 100},
+            {"Toma", 100, 100, 100}
+        };
+
+        もし"typedef"で宣言していれば大丈夫.変数宣言時の"struct"は省略できる.
+        typedef struct Student
+        {
+            char name[16];
+            int jananese, math, english;
+        }Student;
+        cでもc++でも使えるので、互換性を考えるとこっちのほうがいいのかも？！.
+    */
+
     for (int i = 0; i < SIZE_OF_ARRAY(student); ++i)
     {
-        display(student[i]);
+        display_refer(student[i]);
+        display_pointer(&student[i]);
     }
     return 0;
 }
